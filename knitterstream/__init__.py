ab2774b268f3f95edb529a4f5c7f4700af712851
Evan Borgstrom <evan@fatbox.ca>
2012-05-01 16:36:49 -0400

    Addition of initial protocol module, more main work

--- knitterstream/__init__.py
+++ knitterstream/__init__.py
@@ -1,10 +1,63 @@
 from optparse import OptionParser
 
+from knitterstream.protocol import E6000Serial
+
+import time
+import sys
+import os
+
+import logging
+logger = logging.getLogger(__name__)
+
 def main():
-    parser = OptionParser()
-    parser.add_option('-t', '--twitter', dest='twitter',
-            help='Specify a file containing Twitter API credentials')
+    """
+    Main entry point
+    """
+    usage = "usage: %prog -s serial-port -d watch/ [-t ~/.twitter-creds]"
+    parser = OptionParser(usage=usage)
+    parser.add_option('-s', '--serial', dest='serial', metavar='PORT',
+            help='The serial port to use, can be any valid pySerial name')
     parser.add_option('-d', '--dir', dest='dir',
             help='The directory to watch for new files')
+    parser.add_option('-t', '--twitter', dest='twitter', metavar='FILE',
+            help='Specify a file containing Twitter API credentials')
+    parser.add_option('-l', '--log', dest='log', metavar='FILE',
+            help='Log output to a file also')
 
     (options, args) = parser.parse_args()
+
+    # setup logging
+    formatter = logging.Formatter('%(asctime)s %(levelname)s\t%(message)s')
+    logger.setLevel(logging.DEBUG)
+
+    # log to stdout always
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(formatter)
+    logger.addHandler(stdout_handler)
+
+    # do we have a file
+    if options.log:
+        file_handler = logging.FileHandler(options.log)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    if not options.serial:
+        parser.error("You must specify a serial port")
+
+    if not options.dir:
+        parser.error("You must specify a directory to watch")
+
+    if not os.path.isdir(options.dir):
+        parser.error("Invalid directory specified: %s" % options.dir)
+
+    logger.info("KnitterStream - Starting up...")
+
+    serial = E6000Serial(options.serial)
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logger.info("Caught keyboard interrupt, exiting...")
+
+    serial.close()
---
+++ knitterstream/protocol.py
@@ -0,0 +1,45 @@
+import serial
+
+import logging
+logger = logging.getLogger(__name__)
+ 
+class E6000Serial(object):
+    """
+    This class encapsulates the functionality for interfacing with
+    a Passap E6000 via its serial port.
+
+    The E6000 is a VERY simple machine. Too simple infact. Getting
+    the protocol right required me to think as simply as possible.
+
+    For input (what it sends to us) there is only one thing that it
+    sends over the serial port; a single null byte each time the row
+    counter is triggered.
+    
+    For output we need to send a plain text string that is described
+    in the HEADER_FORMAT & DATA_FORMAT templates. The string should
+    be constructed entirely and then written to the serial device in
+    one shot.
+    """
+
+    # These are the templates used for constructing the data we will
+    # send to the E6000.
+    # colour: the number of colours in this pattern
+    # columns: the number of columns in this pattern
+    # rows: the number of rows in this pattern
+    #
+    # The data is a series of "0", "1", "2" or "3" characters (yes
+    # ascii strings, weird) to represent which colour to make the
+    # foreground. There is no RLE or anything, just straight data.
+    HEADER_FORMAT = "%(colour)s %(columns)3s %(rows)3s"
+    DATA_FORMAT = "%(header)s %(data)s\x03"
+
+    def __init__(self, port):
+        logger.info("Initializing E6000 serial interface...")
+        self.serial = serial.Serial(port, 1200,
+                parity=serial.PARITY_EVEN)
+        logger.info(" `-> Initialized!")
+
+    def close(self):
+        logger.info("Closing E6000 serial interface...")
+        self.serial.close()
+        logger.info(" `-> Closed!")
